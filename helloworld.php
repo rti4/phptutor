c7feeebaf0ebedaa3dd0c83864a30235da7d82df
agilleland <bu1ld1ngs1t3S>
2017-04-02 13:11:26 -0400

    Adding the Sass folder

--- helloworld.php
+++ helloworld.php
@@ -8,4 +8,22 @@
 
 echo 'Hello, World!';
 
+$i =0;
+while ($i < 10){
+  $i++;
+}
+
+$i = 0;
+$j =0;
+$header_str = "Header";
+print '<table>';
+print '<tr>';
+
+while ($i < 5){
+  print '<td>'.$i.'</td>';
+  $i++;
+}
+print '</tr>';
+print '</table>';
+
 ?>
