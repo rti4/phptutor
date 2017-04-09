2413d6eda92e2ec724c2a9e8cf64b9b3f012e8af
agilleland <bu1ld1ngs1t3S>
2017-04-09 13:06:27 -0400

    dice challenge from code academy

--- chessboard2.php
+++ chessboard2.php
@@ -7,12 +7,9 @@
  */
 
 
-$alpha_array = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
-$alpha_str = 'ABCDEFGH';
-$header_str = "Header";
-
 function create_chessboard($i) {
 
+  print "<td>$i</td>";
   for ($j = 0; $j < 8; $j++) {
     if ($i % 2 === 0) {
       if ($j % 2 === 0) {
@@ -28,15 +25,35 @@ function create_chessboard($i) {
       }
     }
   }
+  print "<td>$i</td>";
 }
 
+function create_alpharow ($alpha_array) {
+  print '<td>' . ' ' . '</td>';
+  for ($i=0; $i < count($alpha_array); $i++) {
+    print '<td>' . $alpha_array[$i] . '</td>';
+  }
 
+}
+
+#driver code exist here#######
+$alpha_array = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
+$alpha_str = 'ABCDEFGH';
+$header_str = "Header";
 
 print '<table>';
-for ($i = 0; $i < 8; $i++) {
-  print '<tr>';
-    create_chessboard($i);
-  print '</tr>';
-}
+# print letter system
+print '<tr>';
+  create_alpharow($alpha_array);
+print '</tr>';
+  for ($i = 0; $i < 8; $i++) {
+      print '<tr>';
+      # creates the chessboard
+      create_chessboard($i);
+      print '</tr>';
+  }
+print '<tr>';
+  create_alpharow($alpha_array);
+print '</tr>';
 print '</table>';
 ?>
--- dice.php
+++ dice.php
@@ -7,15 +7,22 @@
  */
 
 $rollcount=0;
+$roll = rand(1,6);
 
 do {
+  print $roll;
   $roll=rand(1,6);
   $rollcount++;
+  print $roll;
+  print $rollcount;
+  print '<hr/>';
   if ($roll == 6) {
     echo "You rolled a 6! <br>";
+    break;
   } else {
     echo "Better luck next time <br>";
   }
-} while ($roll == 6);
 
+} while ($roll == 6);
+print $roll;
 echo "You win! It took $rollcount rolls.";
